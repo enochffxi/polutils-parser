953e292d82b210e539008bc8bab59c5cf7e43201
Matthew Scragg <scragg@gmail.com>
2012-08-02 22:59:31 -0500

    setuptools

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = "Parse XML files generated from POLUtils"
+
+with open('README') as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '0.0.1'
+
+setup(
+    name = "POLUtils Parser",
+    version = VERSION,
+    author = "Matthew Scragg",
+    author_email = "scragg@gmail.com",
+    description = DESCRIPTION,
+    license = "MIT",
+    url = "https://github.com/scragg0x/polutils-parser",
+    packages=find_packages(),
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+    test_suite='tests'
+)
